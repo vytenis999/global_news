a6203cbd305730d91232e1ae664e120ef1f30315
Vytenis <vytenis.rope@gmail.com>
2023-05-02 23:00:00 +0300

    Deleted get images method

--- API/Controllers/ArticlesController.cs
+++ API/Controllers/ArticlesController.cs
@@ -155,23 +155,6 @@ namespace API.Controllers
             return Ok(result);
         }
 
-        [HttpPost("getimages")]
-        public IActionResult GetImages([FromBody] string[] names)
-        {
-            var directory = new DirectoryInfo(uploadPath);
-
-            // Filter out any invalid file extensions
-            var validFiles = directory.GetFiles()
-                .Where(file => allowedExtensions.Contains(file.Extension))
-                .Where(file => names.Contains(file.Name));
-
-            // Return a FileResult for each valid file
-            var fileResults = validFiles.Select(file =>
-                new PhysicalFileResult(file.FullName, $"image/{file.Extension.Replace(".", "")}"));
-
-            return new JsonResult(fileResults);
-        }
-
         [HttpDelete("deleteimages")]
         public async Task<IActionResult> DeleteImages(List<string> fileNames)
         {
