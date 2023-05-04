d7453bc1ddcfa043977bdd22551bf54ebbe7eb67
Vytenis <vytenis.rope@gmail.com>
2023-05-04 15:33:17 +0300

    Fixed token interceptor

--- client/src/app/core/interceptors/token.interceptor.ts
+++ client/src/app/core/interceptors/token.interceptor.ts
@@ -4,18 +4,28 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders } from '@angular
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
+  private excludeUrls: string[] = ['https://api.openweathermap.org', 'https://nominatim.openstreetmap.org'];
+
   private addTokenToHeader(request: HttpRequest<any>): HttpRequest<any> {
     const token = localStorage.getItem('token');
-    return request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+    if (token && !this.shouldExclude(request.url)) {
+      return request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    } else {
+      return request;
+    }
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     request = this.addTokenToHeader(request);
     return next.handle(request);
+  }
+
+  private shouldExclude(url: string): boolean {
+    return this.excludeUrls.some(excludedUrl => url.includes(excludedUrl));
   }
 
 }
